63fbf5931584365be2d477c490eb3e3933a0fcf6
Rodrigo Gonzalez <rodrigo.gonzaleztr@outlook.com>
2022-12-14 21:05:11 -0300

    Se realiza correccion de funciones y funcionamiento de la app

--- assets/app/script.js
+++ assets/app/script.js
@@ -1,88 +1,117 @@
-//Elementos html
+/*Elementos y variables html*/
 const formulario = document.querySelector('#formulario');
 const inputTarea = document.querySelector('#inputTarea');
-//const statusTarea = document.querySelector('#statusTarea');
 const tareasContainer = document.querySelector('#tareasContainer');
 const cantidadTareas = document.querySelector('#cantidadTareas');
 const cantidadTareasRealizadas = document.querySelector('#cantidadTareasRealizadas');
 
 
 let listaDeTareas = [
+
     { id: +String(Date.now() + 1).slice(7), descripcionTarea: 'Salir a regar', estado: false },
     { id: +String(Date.now() + 2).slice(7), descripcionTarea: 'Ir a comer', estado: false },
     { id: +String(Date.now() + 3).slice(7), descripcionTarea: 'Hacer las tareas', estado: false },
     { id: +String(Date.now() + 4).slice(7), descripcionTarea: 'Practicar estudios', estado: false },
 
 ];
-console.log(listaDeTareas);
-
-
-// const isChecked = () => {
-//     for (const tarea of listaDeTareas) {
-//         if (tarea.estado === true ) {
-
-//             statusTarea.checked;
-//         }
-//     }
-// }
 
 const renderizaTareas = () => {
+
+    /* Este fragmento de código pinta la cantidad de tareas */
     let totalTareas = listaDeTareas.length;
     cantidadTareas.innerHTML = '';
-    console.log('totaltareas:', totalTareas);
     cantidadTareas.innerHTML += `<p id="cantidadTareas">Total tareas: <b>${totalTareas}</b></p>`;
 
-
+    /* Este fragmento cuenta tiene un contador, luego se evalua la cantidad de tareas realizadas con un forOf */
     totalTareasRealizadas = 0;
     for (const tarea of listaDeTareas) {
-        if (tarea.estado===true) {
-            totalTareasRealizadas+=1;
+        if (tarea.estado === true) {
+            totalTareasRealizadas += 1;
         }
     }
-
-    cantidadTareasRealizadas.innerHTML='';
-    cantidadTareasRealizadas.innerHTML+=`<p id="cantidadTareasRealizadas">Tareas realizadas: <b>${totalTareasRealizadas}</b></p>`;
+    cantidadTareasRealizadas.innerHTML = '';
+    cantidadTareasRealizadas.innerHTML += `<p id="cantidadTareasRealizadas">Tareas realizadas: <b>${totalTareasRealizadas}</b></p>`;
 
 }
 
 const renderizado = () => {
-    // renderizaTareas();
-    let totalTareas = listaDeTareas.length;
-
 
     tareasContainer.innerHTML = '';
-
+    /* Se recorren todas las tareas del arreglo 'listaDeTareas' */
     listaDeTareas.forEach(element => {
-        tareasContainer.innerHTML += `
-        <div class="container text-center">
-            <div class="row">
-                <div class="col">
-                    ${element.id}
+
+        /* Se evalua el estado de cada tarea, en caso de ser false no se agrega el atributo checked a la tarea */
+        //! Se le asigna una función en 'onClick' del checkbox, al momneto de clickearlo, se marcará 'checked'
+        //!
+        if (element.estado === false) {
+            tareasContainer.innerHTML += `
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        ${element.id}
+                    </div>
+                    <div class="col">
+                        ${element.descripcionTarea}
+                    </div>
+                    <div class="col">
+                        <input onclick="tareaChecker(${element.id})" type="checkbox" name="" id="${element.id}">
+                    </div>
+                    <div class="col">
+                        <button onclick="elimina(${element.id})" class="eliminar-button" id="${element.id}">❌</button>
+                    </div>
                 </div>
-                <div class="col">
-                    ${element.descripcionTarea}
+            </div>
+            `;
+        } else {
+            tareasContainer.innerHTML += `
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        ${element.id}
+                    </div>
+                    <div class="col">
+                        ${element.descripcionTarea}
+                    </div>
+                    <div class="col">
+                        <input onclick="tareaChecker(${element.id})" type="checkbox" name="" id="${element.id}" checked>
+                    </div>
+                    <div class="col">
+                        <button onclick="elimina(${element.id})" class="eliminar-button" id="${element.id}">❌</button>
+                    </div>
                 </div>
-                <div class="col">
-                    <input onclick="modifica(${element.id})" type="checkbox" name="" id="${+String(Date.now()).slice(7)}">
-                </div>
-                <div class="col">
-                    <button onclick="elimina(${element.id})" class="eliminar-button">❌</button>
-                </div>
-        </div>
-        </div>
-        `
+            </div>
+            `;
+        }
+
 
     });
-
     renderizaTareas();
-    // cantidadTareas.innerHTML='';
-    // console.log('totaltareas:', totalTareas);
-    // cantidadTareas.innerHTML+=`<p id="cantidadTareas">Total tareas: <b>${totalTareas}</b></p>`;
-    // isChecked();
-    //console.log(statusTarea.checked);
 }
 
 renderizado();
+
+/* Función para cambiar estado de actividad */
+// !Esta función funciona en el 'onClick' del checkbox
+const tareaChecker = (checkeador) => {
+    
+    /* Para cada tarea, comparo la id de la tarea en el arreglo con la enviada por el checkbox en onClick */
+    listaDeTareas.forEach((tarea) => {
+        if (tarea.id === checkeador) {
+
+            /* Accedemos al estado de la tarea y en caso de ser falso, su estado cambiará a true, en caso contrario cambiará a false */
+            if (tarea.estado === false) {
+                tarea.estado = true;
+                renderizado();
+                renderizaTareas();
+            } else {
+                tarea.estado = false;
+                renderizado();
+                renderizaTareas();
+            }
+        }
+    });
+}
+
 
 /* Función que agrega un elemento */
 const agrega = () => {
@@ -95,8 +124,6 @@ const agrega = () => {
     } else {
         alert('Ingrese una tarea válida');
     }
-    
-
 }
 
 
@@ -104,37 +131,9 @@ const agrega = () => {
 const elimina = (id) => {
     const indice = listaDeTareas.findIndex((elemento) => elemento.id === id);
     listaDeTareas.splice(indice, 1);
-
-    let html = '';
-    for (const tarea of listaDeTareas) {
-        html += `
-        <div class="container text-center">
-            <div class="row">
-                <div class="col">
-                    ${tarea.id}
-                </div>
-                <div class="col">
-                    ${tarea.descripcionTarea}
-                </div>
-                <div class="col">
-                    <input onclick="modifica(${tarea.id})" type="checkbox" name="" id="statusTarea">
-                </div>
-                <div class="col">
-                    <button onclick="elimina(${tarea.id})" class="eliminar-button">❌</button>
-                </div>
-            </div>
-        </div>
-        `;
-    }
-    tareasContainer.innerHTML = html;
+    renderizado();
     renderizaTareas();
 };
-
-
-/* Función para cambiar estado de actividad */
-
-
-
 
 
 /* Botón que envía la info del formulario */
@@ -143,91 +142,9 @@ formulario.addEventListener('submit', (evento) => {
     agrega();
     renderizado();
 
-    inputTarea.value = '';
+    inputTarea.value = '';/* Este fragmento limpia el input luego de ingresar una tarea */
 
 });
 
-
-
-// console.log(Date.now());
-// console.log(Date.now()+1);
-
-
-
-
-
-// const modifica = (idTarea) => {
-//     const tarea = listaDeTareas.find((tarea) => tarea.id === idTarea);
-//     if (tarea) {
-//         if (tarea.estado) {
-//             tarea.estado = false;
-
-//             let html = '';
-//             for (const tarea of listaDeTareas) {
-//                 html += `
-//                 <div class="container text-center">
-//                     <div class="row">
-//                         <div class="col">
-//                             ${tarea.id}
-//                         </div>
-//                         <div class="col">
-//                             ${tarea.descripcionTarea}
-//                         </div>
-//                         <div class="col">
-//                             <input onclick="modifica(${listaDeTareas.id})" type="checkbox" name="" id="status">
-//                         </div>
-//                         <div class="col">
-//                             <button onclick="elimina(${listaDeTareas.id})" class="eliminar-button">❌</button>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 `;
-//             }
-//             tareasContainer.innerHTML = html;
-//         } else {
-//             tarea.estado = true;
-//             let html = '';
-//             for (const tarea of listaDeTareas) {
-//                 html += `
-//                 <div class="container text-center">
-//                     <div class="row">
-//                         <div class="col">
-//                             ${tarea.id}
-//                         </div>
-//                         <div class="col">
-//                             ${tarea.descripcionTarea}
-//                         </div>
-//                         <div class="col">
-//                             <input onclick="modifica(${listaDeTareas.id})" type="checkbox" name="" id="status" checked>
-//                         </div>
-//                         <div class="col">
-//                             <button onclick="elimina(${listaDeTareas.id})" class="eliminar-button">❌</button>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 `;
-//             }
-//             tareasContainer.innerHTML = html;
-//         }
-//     }
-
-
-// }
-
-
-const modifica = (id) => {
-    console.log('modificando');
-
-    listaDeTareas.map((elem) => {
-        if (elem.id === id) {
-            if (elem.estado === false) {
-                return elem.estado = true;
-            } else {
-                return elem.estado = false;
-            }
-        }
-    })
-
-}
 
 renderizaTareas();
